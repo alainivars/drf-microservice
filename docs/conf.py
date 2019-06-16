e10e3e91fe551ff502716dde7508a4957cccb942
Alain IVARS <alainivars@gmail.com>
2019-06-16 20:41:04 +0100

    Make static doc more modular & less duplicated, fix the ImportMismatchError in tox and fix template for docs

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,9 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = [
+    '/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx',
+    'templates', '_templates', '.templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
