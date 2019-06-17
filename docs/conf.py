f86e3a6fede06e177c98c130cbcea1a78b7fe207
Alain IVARS <alainivars@gmail.com>
2019-06-17 14:09:47 +0100

    fix for readthedoc for Single-sourcing the package version.

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,10 @@
 import os
 import sys
 
-from docs import version
-
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+
+from docs import version
 
 
 # -- Project information -----------------------------------------------------
