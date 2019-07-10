a969324740874cf2f0cfb3cbfbad6c57fa8b4ae8
alainivars <alainivars@gmail.com>
2019-07-10 15:02:08 +0100

    add missing STATIC_ROOT

--- my_api/config/common.py
+++ my_api/config/common.py
@@ -233,6 +233,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATIC_URL = '/static/'
 
 # Some media files if you need it else remove it
